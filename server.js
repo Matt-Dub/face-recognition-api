3d47bdd7a1f834d377386b38f0fb99182ea80a7d
Mathieu <Matt@soundfingers.com>
2025-06-12 10:13:18 +0200

    ...removed extra /

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   methods: 'GET,POST, PUT',
   credentials: true
 }));
